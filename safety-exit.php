140077a6565a1cc90caa48a3c2720f22872c08f9
honekun <honekun@gmail.com>
2020-11-03 09:44:30 -0600

    Update safety-exit.php
    Added temporary fix for WordPress installs that are based on composer, the used relative path code was not accurate across different WordPress installs, preventing plugin to be used.

--- safety-exit.php
+++ safety-exit.php
@@ -11,7 +11,7 @@
  * Plugin Name: Safety Exit
  * Plugin URI:
  * Description: A safety exit plugin
- * Version:     1.4.3
+ * Version:     1.4.4
  * Author:      Tomas Cordero
  * Author URI:  https://tomascordero.com
  * License:     GPL-2.0+
@@ -19,41 +19,49 @@
  * Text Domain: safety-exit
  */
 
+$path = realpath(dirname(__FILE__) . '/../../../')."/wp-includes/pluggable.php";
 
+// This adds support for bedrock, ABSPATH is set by bedrock as part of the config.
+if( defined( 'ABSPATH' ) ) {
+    $path = ABSPATH . '/wp-includes/pluggable.php';
+}
 
 $errors = false;
 // Check to see if required file exists. If not do not initialize plugin
 try {
-	if( !file_exists(realpath(dirname(__FILE__) . '/../../../')."/wp-includes/pluggable.php") ) {
-		throw new Exception ('Unable to load pluggable.php in: ' . realpath(dirname(__FILE__) . '/../../../')."/wp-includes/pluggable.php" . ' Safety Exit is disabled until this error is fixed.');
-	}else{
-		require_once(realpath(dirname(__FILE__) . '/../../../')."/wp-includes/pluggable.php");
+	if ( ! file_exists( $path ) ) {
+		throw new Exception ( 'Unable to load pluggable.php in: ' . $path . ' Safety Exit is disabled until this error is fixed.' );
+	} else {
+		require_once( $path );
 	}
-}catch(Exception $e){
-	if ( is_admin() ){
-	?>
-	<div class="error notice">
-		<p><?= $e->getMessage(); ?></p>
-	</div>
-	<?php
+} catch( Exception $e ) {
+	if ( is_admin() ) {
+		?>
+		<div class="error notice">
+			<p><?php echo esc_html( $e->getMessage() ); ?></p>
+		</div>
+		<?php
 	}
 	$errors = true;
 }
+
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
 foreach ( glob( plugin_dir_path( __FILE__ ) . 'lib/*.php' ) as $file ) {
     include_once $file;
 }
-if( !$errors ) {
+
+if ( ! $errors ) {
 	if ( is_admin() ){
-		if(current_user_can('administrator')){
-			$admin = new Safety_Exit_Admin(__FILE__);
+		if( current_user_can( 'administrator' ) ) {
+			$admin = new Safety_Exit_Admin( __FILE__ );
 			$admin->init();
 		}
 	} else {
-		$frontend = new Safety_Exit_Frontend(__FILE__);
+		$frontend = new Safety_Exit_Frontend( __FILE__ );
 		$frontend->init();
 	}
 }
